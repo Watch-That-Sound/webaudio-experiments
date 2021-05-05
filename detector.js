5285f35b00d2a45f969d7aeb702d364ccce65b69
Jan Misker <jan@misker.nl>
2021-05-05 22:30:54 +0200

    audioworklet testing

---
+++ detector.js
@@ -0,0 +1,28 @@
+class DetectorProcessor extends AudioWorkletProcessor {
+  constructor(...args) {
+    super(...args);
+    console.log("inside detector", args);
+    this.port.onmessage = (e) => {
+      console.log(e.data);
+      this.detectMute = e.data.detect === "mute";
+      this.startTime = Date.now();
+    };
+  }
+  process(inputs, outputs, params) {
+    // console.log(this.startTime, inputs.length);
+    const sumSqr = inputs[0][0].reduce((sumSqr, val) => sumSqr + val * val, 0);
+    const rms = Math.sqrt(sumSqr / 128);
+    this.port.postMessage({ volume: rms });
+    if (this.startTime) {
+      if (this.detectMute) {
+        if (rms < 0.1) {
+          console.log("found silence", sumSqr, rms);
+          this.startTime = undefined;
+        }
+      }
+    }
+    return true;
+  }
+}
+
+registerProcessor("detector", DetectorProcessor);
